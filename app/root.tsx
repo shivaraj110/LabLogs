12b7513f332a4daeed61aeea6491b9418101749b
shivaraj-kc <shivarajchandaragi9@gmail.com>
2024-09-08 01:34:41 +0530

    styled landing page

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { FontFamily } from "@fontsource/poppins";
 import "./tailwind.css";
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -17,13 +18,37 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        <header className="pb-4 pl-6 pt-3 bg-slate-100  shadow-md">
+        <header className="pb-20 pt-8 pl-36 ">
           <nav>
             <Link to={"/"}>
-              <button>HOME</button>
+              <button className="font-extrabold pt-1 tracking-wider text-gray-700 text-3xl">
+                Lablogs
+              </button>
             </Link>
             <Link to={"/"}>
-              <button className="pl-8  pt-2">ABOUT</button>
+              <button className="pl-12 font-semibold  text-gray-500 text-md">
+                About
+              </button>
+            </Link>
+            <Link to={"/"}>
+              <button className="pl-12 font-semibold  text-gray-500 text-md">
+                Features
+              </button>
+            </Link>
+            <Link to={"/"}>
+              <button className="pl-12 font-semibold  text-gray-500 text-md">
+                Contact
+              </button>
+            </Link>
+            <Link to={"/"}>
+              <button className="pl-80 font-semibold pr-5  text-gray-500 text-md">
+                Login
+              </button>
+            </Link>
+            <Link to={"/"}>
+              <button className=" p-1 font-semibold rounded-2xl signupBtn text-gray-500 text-md">
+                Signup
+              </button>
             </Link>
           </nav>
         </header>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,8 +10,32 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div>
-      <Link to={"/signup"}> signup </Link>
-      <Link to={"/subjects"}>Subjects</Link>
+      {/* <div className="font-sans p-4">home</div> */}
+      {/* <Link to={"/signup"}> signup </Link> */}
+      {/* <Link to={"/subjects"}>Subjects</Link> */}
+      <div className="grid grid-cols-2">
+        <div>
+          <div className="text-5xl font-extrabold text-gray-700 pl-28">
+            Manage your programming lab assignments
+          </div>
+          <div className="text-lg text-gray-400 font-semibold pl-28 pb-5 pt-3">
+            Add and update your assignments collection and access them from
+            anywhere.
+          </div>
+          <Link to={"/"}>
+            <button className=" p-1.5 w-10 font-semibold rounded-2xl startBtn text-gray-500 text-md">
+              Get Started
+            </button>
+          </Link>
+        </div>
+        <div className="">
+          <img
+            src="./public/working.svg"
+            alt="working svg"
+            className="pb-28 ml-32"
+          />
+        </div>
+      </div>
     </div>
   );
 }
