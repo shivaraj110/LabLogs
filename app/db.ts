cb63a69aab189b8a8668dd5541ca79e542bb76f5
shivaraj-kc <shivarajchandaragi9@gmail.com>
2024-09-16 12:29:21 +0530

    trying to deploy...

--- app/db.ts
+++ app/db.ts
@@ -1,5 +1,4 @@
-import { PrismaClient } from "@prisma/client"
-const prisma  = new PrismaClient()
+import { prisma } from "./routes/subjects.$userId"
 type assignmentPayload = {
     title : string,
     description : string,
